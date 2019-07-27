ab0874df6edac1cf2aeffa8864567c66052ccafd
minblock <sev@provgn.com>
2019-07-27 08:44:34 +0000

    Permissions for exceution

--- updateall.sh
+++ updateall.sh
@@ -4,7 +4,7 @@ wget https://github.com/minblock/picscoin/releases/download/v0.17.9/picscoin-0.1
 tar -xvf picscoin-0.17.9-x86_64-linux-gnu.tar.gz
 wget https://github.com/minblock/ogkush/releases/download/v0.17.9.1/ogkush-0.17.9-x86_64-linux-gnu.tar.gz
 tar -xvf ogkush-0.17.9-x86_64-linux-gnu.tar.gz
-wget https://github.com/minblock/compute/releases/download/v0.14.0.7/computecore-0.14.0-x86_64-linux-gnu.tar.gz
+wget https://github.com/minblock/compute/releases/download/v0.14.0.5/computecore-0.14.0-x86_64-linux-gnu.tar.gz
 tar -xvf computecore-0.14.0-x86_64-linux-gnu.tar.gz
 mkdir ~/bin/
 mv picscoin-0.17.9/bin/* ~/bin
@@ -12,7 +12,7 @@ mv ogkush-0.17.9/bin/* ~/bin
 mv computecore-0.14.0/bin/* ~/bin
 
 rm -f computecore-0.14.0-x86_64-linux-gnu.tar.gz
-rm -rf computecore-0.14.0/
+rm -rf computecore-0.14.0.5/
 rm -f ogkush-0.17.9-x86_64-linux-gnu.tar.gz
 rm -rf ogkush-0.17.9
 rm -f picscoin-0.17.9-x86_64-linux-gnu.tar.gz
