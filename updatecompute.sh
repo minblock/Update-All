408d39bab56a2f8bdef749f7543f0a3d01a0412c
stevecat <38344452+minblock@users.noreply.github.com>
2019-07-28 22:40:00 -0400

    Create updatecompute.sh

---
+++ updatecompute.sh
@@ -0,0 +1,7 @@
+mkdir ~/tempute
+cd ~/tempute
+https://github.com/minblock/compute/releases/download/v0.14.6.1-RC/computecore-0.14.6-x86_64-linux-gnu.tar.gz
+tar -xvf computecore-0.14.6-x86_64-linux-gnu.tar.gz
+mkdir ~/bin/
+mv computecore-0.14.6/bin/* ~/bin
+cd ../;rm -rf ~/tempute
