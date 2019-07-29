ad129f3120b53b3637453edc0b77512f5ef72eb6
stevecat <38344452+minblock@users.noreply.github.com>
2019-07-28 23:22:09 -0400

    Update updateall.sh

--- updateall.sh
+++ updateall.sh
@@ -4,7 +4,8 @@ wget https://github.com/minblock/picscoin/releases/download/v0.17.9/picscoin-0.1
 tar -xvf picscoin-0.17.9-x86_64-linux-gnu.tar.gz
 wget https://github.com/minblock/ogkush/releases/download/v0.17.9.1/ogkush-0.17.9-x86_64-linux-gnu.tar.gz
 tar -xvf ogkush-0.17.9-x86_64-linux-gnu.tar.gz
-wget https://github.com/minblock/compute/releases/download/v0.14.6/computecore-0.14.6-x86_64-linux-gnu.tar.gz
+wget https://github.com/minblock/compute/releases/download/v0.14.6.1-RC/computecore-0.14.6-x86_64-linux-gnu.tar.gz
+echo "f07e28f7912418f06b77b72dbbad929a227e84d22f3b3d5ca7abc5369319fc7a  computecore-0.14.6-x86_64-linux-gnu.tar.gz" | sha256sum -c
 tar -xvf computecore-0.14.6-x86_64-linux-gnu.tar.gz
 mkdir ~/bin/
 mv picscoin-0.17.9/bin/* ~/bin
