0f6ae2a43b774bd7f60c66e4bd1bc5d3d9eac595
stevecat <38344452+minblock@users.noreply.github.com>
2019-09-21 20:49:26 -0400

    Update updateall.sh

--- updateall.sh
+++ updateall.sh
@@ -11,6 +11,6 @@ echo "f07e28f7912418f06b77b72dbbad929a227e84d22f3b3d5ca7abc5369319fc7a  computec
 tar -xvf computecore-0.14.6-x86_64-linux-gnu.tar.gz
 mkdir ~/bin/
 mv picscoin-0.17.9/bin/* ~/bin
-mv ogkush-0.17.9/bin/* ~/bin
+mv ogkush-0.17.10/bin/* ~/bin
 mv computecore-0.14.6/bin/* ~/bin
 cd ../;rm -rf ~/temp6549
