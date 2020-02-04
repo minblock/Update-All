00b2a3bf13f9302b9a27260840cabcc405256875
stevecat <38344452+minblock@users.noreply.github.com>
2020-02-03 20:40:13 -0500

    Update updatesate.sh

--- updatesate.sh
+++ updatesate.sh
@@ -3,7 +3,7 @@ mkdir ~/tmp/;mkdir ~/tmp/1337; mkdir ~/tmp/1337/sate346
 cd ~/tmp/1337/sate346
 ~/bin/satellite-cli stop
 wget https://github.com/minblock/satellite/releases/download/v0.17.1.3/satellite-0.17.1-aarch64-linux-gnu.tar.gz
-echo "1215ba3e60dc72af34949b30f9cec1811f2d07b0dbe29936e5d9ec552ea29404 satellite-0.17.1-aarch64-linux-gnu.tar.gz" | sha256sum -c
+echo "0289dcfd455e81f491856a588cb551c48acd85248c55f29c854514b4973f60a4 satellite-0.17.1-aarch64-linux-gnu.tar.gz" | sha256sum -c
 echo This should say OK if the file was unmodified during transmission
 tar -xvf satellite-0.17.1-aarch64-linux-gnu.tar.gz
 mkdir ~/bin
