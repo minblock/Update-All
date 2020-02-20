88d042fae3370372134eb1ad55a996f9a43d9986
stevecat <38344452+minblock@users.noreply.github.com>
2020-02-20 08:54:45 -0500

    Update updateall.sh

--- updateall.sh
+++ updateall.sh
@@ -2,7 +2,7 @@ echo SHA256 sums should read OK.
 read -p "All bitcoins will be shutdown gracefully Press [Enter] key to start update or CTRL+C to terminate this process..."
 bash updatejoto.sh
 bash updatecompute.sh
-bash updateluacoin.sh
+bash updatelua.sh
 bash updatekush.sh
 bash updatesatellite.sh
 bash updatemaji.sh
