f58ec46eebaee3edbf516d26ed41a2e706e99239
stevecat <38344452+minblock@users.noreply.github.com>
2020-02-24 00:23:07 -0500

    Create setup.sh
    Allow incoming ports and change default litecoin data directory

---
+++ setup.sh
@@ -0,0 +1,28 @@
+alias compute="sudo ufw allow 11337/tcp"
+alias picscoin="sudo ufw allow 1337/tcp"
+alias ogkush="sudo ufw allow 2337/tcp"
+alias majestees="sudo ufw allow 9999/tcp"
+alias litecoin="sudo ufw allow 1837/tcp"
+alias joto="sudo ufw allow 5555/tcp"
+echo enter your username:$username
+echo $username >> username
+$litecoin=$HOME/.litercoin
+echo lua
+8808/tcp
+echo satellite
+6666/tcp
+echo 11337 
+compute
+echo picscoin
+picscoin
+echo ogkush
+ogkush
+echo joto
+joto
+echo majestees
+majestees
+echo litecoin
+litecoin
+mkdir ~/.litercoin
+touch ~/.litercoin/litecoin.conf
+echo data-dir=$litercoin >> $HOME/.litercoin/litecoin.conf
