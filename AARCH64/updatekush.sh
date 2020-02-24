d7c5a0c774cd32513800bd329593987da7465428
qsteesb <sev@provgn.com>
2020-02-23 20:22:00 -0500

    AARCH64 updateall

---
+++ AARCH64/updatekush.sh
@@ -0,0 +1,10 @@
+ogkush-cli stop
+mkdir ~/temp420
+cd ~/temp420
+wget https://github.com/minblock/ogkush/releases/download/v0.17.10.2/ogkush-0.17.10-aarch64-linux-gnu.tar.gz
+echo "db0566e47654291947204118cf010f25d86172a599334f099bb691a175fc94e8  ogkush-0.17.10-aarch64-linux-gnu.tar.gz" | sha256sum -c
+tar -xvf ogkush-0.17.10-aarch64-linux-gnu.tar.gz
+mkdir ~/bin/
+mv ogkush-0.17.10/bin/* ~/bin
+cd ../;rm -rf ~/temp420
+~/bin/ogkushd -daemon
---
+++ AARCH64/updatelightercoin.sh
@@ -0,0 +1,13 @@
+echo 0.18.1 Update
+mkdir ~/tmp/;mkdir ~/tmp/1337; mkdir ~/tmp/1337/lite346
+cd ~/tmp/1337/lite346
+~/bin/satellite-cli stop
+wget https://github.com/minblock/litecoin/releases/download/v0.18.1-rc/litecoin-0.18.1-aarch64-linux-gnu.tar.gz
+echo "b3b5faea45b6c61da036bdfbd7a5b2a7a66fcdf54374ca95ae02106348d9057b litecoin-0.18.1-aarch64-linux-gnu.tar.gz" | sha256sum -c
+echo This should say OK if the file was unmodified during transmission
+tar -xvf litecoin-0.18.1-aarch64-linux-gnu.tar.gz
+mkdir ~/bin
+mv litecoin-0.18.1/bin/* ~/bin
+~/bin/litecoind -daemon
+cd ~
+rm -rf ~/tmp/1337/lite346
