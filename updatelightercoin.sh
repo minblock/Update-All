284e381bcc5e4c73915913a2e4600399311ffdf6
stevecat <38344452+minblock@users.noreply.github.com>
2020-02-27 01:02:33 -0500

    Updated checkpoints

--- updatelightercoin.sh
+++ updatelightercoin.sh
@@ -1,13 +1,13 @@
 echo 0.18.1 Update
 mkdir ~/tmp/;mkdir ~/tmp/1337; mkdir ~/tmp/1337/lite346
 cd ~/tmp/1337/lite346
-~/bin/satellite-cli stop
-wget https://github.com/minblock/litecoin/releases/download/v0.18.1-rc/litecoin-0.18.1-x86_64-linux-gnu.tar.gz
-echo "b3b5faea45b6c61da036bdfbd7a5b2a7a66fcdf54374ca95ae02106348d9057b litecoin-0.18.1-x86_64-linux-gnu.tar.gz" | sha256sum -c
+~/bin/litecoin-cli stop
+wget https://github.com/minblock/litecoin/releases/download/v0.18.2.0.2/litecoin-0.18.2rc2-x86_64-linux-gnu.tar.gz
+echo "ec1186656adabdbbdf8570de4a50485a4ae8d1c1a64884bf74667b16c67ab471 litecoin-0.18.2rc2-x86_64-linux-gnu.tar.gz" | sha256sum -c
 echo This should say OK if the file was unmodified during transmission
-tar -xvf litecoin-0.18.1-x86_64-linux-gnu.tar.gz
+tar -xvf litecoin-0.18.2rc2-x86_64-linux-gnu.tar.gz
 mkdir ~/bin
-mv litecoin-0.18.1/bin/* ~/bin
+mv litecoin-0.18.2/bin/* ~/bin
 ~/bin/litecoind -daemon
 cd ~
 rm -rf ~/tmp/1337/lite346
