c871c3651c6862675838b6f30228b7607851208c
stevecat <38344452+minblock@users.noreply.github.com>
2020-03-03 21:44:55 -0500

    Update ufwfirewalls.sh

--- ufwfirewalls.sh
+++ ufwfirewalls.sh
@@ -6,3 +6,11 @@ sudo ufw allow 1837/tcp
 sudo ufw allow 5555/tcp
 sudo ufw allow 8808/tcp
 sudo ufw allow 6666/tcp
+sudo iptables -A INPUT -p tcp --dport 11337 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 1337 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 2337 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 9999 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 5555 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 1837 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 8808 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 6666 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
