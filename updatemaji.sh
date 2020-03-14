f60bb1bf84bdd08c208b76ce3229a478214cce89
stevecat <38344452+minblock@users.noreply.github.com>
2020-03-14 18:00:10 -0400

    Update updatemaji.sh

--- updatemaji.sh
+++ updatemaji.sh
@@ -1,7 +1,7 @@
 echo 0.18.1 Update
 mkdir ~/tmp/;mkdir ~/tmp/1337; mkdir ~/tmp/1337/majii888
 cd ~/tmp/1337/majii888
-~/bin/satellite-cli stop
+~/bin/majestees-cli stop
 wget https://github.com/minblock/majestees/releases/download/v0.18.1.6/majestees-0.18.1.1rc6-x86_64-linux-gnu.tar.gz
 echo "9be14e7312d52e1bd69a774d2d80530fa58b1db070cd14bfdf306434aa71dc9b majestees-0.18.1.1rc6-x86_64-linux-gnu.tar.gz" | sha256sum -c
 echo This should say OK if the file was unmodified during transmission
