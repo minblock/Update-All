f2552fca0f1fbbadfd5f041ba52d035b10d486d7
stevecat <38344452+minblock@users.noreply.github.com>
2020-03-20 20:22:42 -0400

    Update updatepicscoin.sh

--- AARCH64/updatepicscoin.sh
+++ AARCH64/updatepicscoin.sh
@@ -2,12 +2,12 @@ echo 0.18.1 Update
 mkdir ~/tmp/;mkdir ~/tmp/1337; mkdir ~/tmp/1337/picsco1n
 cd ~/tmp/1337/picsco1n
 ~/bin/picscoin-cli stop
-wget https://github.com/minblock/picscoin/releases/download/v0.18.1-RC/picscoin-0.18.1-aarch64-linux-gnu.tar.gz
-echo "927eedecef808be0bc68debba80dfa4bd6b2529b7dc87d9847c7cf5ea4551d26  picscoin-0.18.1-aarch64-linux-gnu.tar.gz" | sha256sum -c
+wget https://github.com/minblock/picscoin/releases/download/v0.18.1.5.1/picscoin-0.18.1.5rc1-aarch64-linux-gnu.tar.gz
+echo "a970045c9cf7d095c8ea3e1152eab77407a238bc9383d697a2bf49c7968693b3  picscoin-0.18.1.5rc1-aarch64-linux-gnu.tar.gz" | sha256sum -c
 echo This should say OK if the file was unmodified during transmission
-tar -xvf picscoin-0.18.1-aarch64-linux-gnu.tar.gz
+tar -xf picscoin-0.18.1.5rc1-aarch64-linux-gnu.tar.gz
 mkdir ~/bin
-mv picscoin-0.18.1/bin/* ~/bin
+mv picscoin-0.18.1.5rc1/bin/* ~/bin
 ~/bin/picscoind -daemon
 cd ~
 rm -rf ~/tmp/1337/picsco1n
