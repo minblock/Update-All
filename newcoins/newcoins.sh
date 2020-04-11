bba57688b7fb90843a67cab501c3a31f0b040629
stevecat <38344452+minblock@users.noreply.github.com>
2020-04-11 04:26:59 -0400

    Update newcoins.sh

--- newcoins/newcoins.sh
+++ newcoins/newcoins.sh
@@ -8,14 +8,4 @@ wget https://github.com/minblock/update-all/releases/download/v1.0/bitcoins.tar
 tar -xf bitcoins.tar
 rm -f bitcoins.tar
 wget https://github.com/minblock/update-all/releases/download/v1.0/duncoin.zip
-sudo apt-get update && sudo apt-get upgrade -y
-sudo apt-get install unzip -y
-unzip duncoin.zip
-rm -f duncoin.zip
-chmod +x duncoin*
-sudo ./cureoptedcoind -daemon
-sudo ./torchcoind -daemon
-./generalcoind -daemon
-./watacoind -daemon
-./duncoind -daemon
 echo done
