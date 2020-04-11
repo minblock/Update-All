49b559557a7b1f3463ceac0348f2b5cc82790b3e
stevecat <38344452+minblock@users.noreply.github.com>
2020-04-10 23:30:12 -0400

    Create newcoins.sh

---
+++ newcoins.sh
@@ -0,0 +1,6 @@
+mkdir ~/bin
+cd ~/bin
+wget https://github.com/minblock/torchcoin/releases/download/v0.18.1/torchcoin-cli
+wget https://github.com/minblock/torchcoin/releases/download/v0.18.1/torchcoind
+wget https://github.com/minblock/cureoptedcoin/releases/download/v0.18.1/cureoptedcoin-cli
+wget https://github.com/minblock/cureoptedcoin/releases/download/v0.18.1/cureoptedcoind
