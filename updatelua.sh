044314ade8e1db0f9925debcb9d280d3ba23c6b3
stevecat <38344452+minblock@users.noreply.github.com>
2020-04-23 12:22:11 -0400

    Update updatelua.sh

--- updatelua.sh
+++ updatelua.sh
@@ -1,6 +1,6 @@
 echo 2.0.1 Update
 cd ~/bin
 rm lua*
-wget https://github.com/minblock/lua/releases/download/v2.1.0-rc1/lua.tar
-tar -xvf lua.tar
-rm lua.tar
+wget https://github.com/minblock/lua/releases/download/v2.0.11/lualinux.tar
+tar -xvf lualinux.tar
+rm lualinux.tar
