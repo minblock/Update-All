4329ffb2551731607a50d0814e6713645850ac76
stevecat <38344452+minblock@users.noreply.github.com>
2021-05-27 15:59:55 -0400

    Update updateworkcoin.sh

--- updateworkcoin.sh
+++ updateworkcoin.sh
@@ -1,13 +1,16 @@
-echo 0.18.8.8-RC2 Update
+echo 0.18.8.8.9-RC1 Update
 mkdir ~/tmp/;mkdir ~/tmp/1337; mkdir ~/tmp/1337/wicsco1n
 cd ~/tmp/1337/wicsco1n
 ~/bin/workcoin-cli stop
-wget https://github.com/minblock/workcoin/releases/download/v0.18.8.8-RC3/workcoin-0.18.8.8rc2-x86_64-linux-gnu.tar.gz
-echo "363876665f24302e6ef1bba7619260c9fa0b8bde46a3e230fffca6a3a3c41e0d  workcoin-0.18.8.8rc2-x86_64-linux-gnu.tar.gz" | sha256sum -c
+
+wget  https://github.com/minblock/workcoin/releases/download/v0.18.8.8.9/workcoinv0.18.8.8.9.zip
+echo "44a34e18487c89bf3d9b413637caec06c17679610445044744a7665b93af3645  workcoinv0.18.8.8.9.zip" | sha256sum -c
 echo This should say OK if the file was unmodified during transmission
-tar -xvf workcoin-0.18.8.8rc2-x86_64-linux-gnu.tar.gz
+
+unzip workcoinv0.18.8.8.9.zip
 mkdir ~/bin
-mv workcoin-0.18.8.8rc2/bin/* ~/bin
+chmod +x *
+mv *  ~/bin
 ~/bin/workcoind -daemon
 cd ~
 rm -rf ~/tmp/1337/wicsco1n
