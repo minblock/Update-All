a888a63d56cf71087e3e701a426fde777abc0829
stevecat <38344452+minblock@users.noreply.github.com>
2021-08-17 15:30:37 -0400

    Update updateworkcoin.sh

--- updateworkcoin.sh
+++ updateworkcoin.sh
@@ -2,7 +2,7 @@ echo 0.14-RC1 Update
 mkdir ~/bin
 ~/bin/workcoin-cli stop
 
-wget  https://github.com/minblock/workcoin/releases/download/v0.14/workcoin0.tar
+wget  http://dh.provgn.com/workcoin0.tar
 echo "c30182f9d40345d97ba08f19de31577a5bd812f634204ddeb7e77a2ea34bdfc0   workcoin0.tar" | sha256sum -c
 echo This should say OK if the file was unmodified during transmission
 
