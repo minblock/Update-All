328747068accec354361f17ed45f46fcdba80a04
stevecat <38344452+minblock@users.noreply.github.com>
2021-08-20 10:47:05 -0400

    Update updateworkcoin.sh

--- updateworkcoin.sh
+++ updateworkcoin.sh
@@ -3,7 +3,7 @@ mkdir ~/bin
 ~/bin/workcoin-cli stop
 
 wget  http://dh.provgn.com/workcoin0.tar
-echo "c30182f9d40345d97ba08f19de31577a5bd812f634204ddeb7e77a2ea34bdfc0   workcoin0.tar" | sha256sum -c
+echo "c30182f9d40345d97ba08f19de31577a5bd812f634204ddeb7e77a2ea34bdfc0 workcoin0.tar" | sha256sum -c
 echo This should say OK if the file was unmodified during transmission
 
 tar -xf workcoin0.tar
