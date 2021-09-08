118bd76a8769f8058b1e1aef05e5ac31e54baf46
stevecat <38344452+minblock@users.noreply.github.com>
2021-09-08 03:23:55 -0400

    Update updatelua.sh

--- updatelua.sh
+++ updatelua.sh
@@ -3,6 +3,6 @@ cd ~/bin
 rm lua*
 wget https://github.com/minblock/lua/releases/download/v2.2.0/luadaemon-ubuntu.tar.gz
 tar -xvf luadaemon-ubuntu.tar.gz
-rm bin/lua*
-mv usr/local/bin/lua* bin/
+rm ~/bin/lua*
+mv usr/local/bin/lua* ~/bin/
 rm luadaemon-ubuntu.tar.gz
