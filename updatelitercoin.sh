c725e0201e7978939e0e59b8b211d8166e4a5da7
stevecat <38344452+minblock@users.noreply.github.com>
2021-11-02 06:01:36 -0400

    Update updatelitercoin.sh

--- updatelitercoin.sh
+++ updatelitercoin.sh
@@ -4,5 +4,5 @@ echo "9bd158a1e24b6b3c648a4a981aa350bec04b3ac0c7c4dfd3ba338da1ec176de2  Litercoi
 echo This should say OK if the file was unmodified during transmission
 tar -xvf Litercoin-v0.18.1.tar.gz
 rm -rf  Litercoin-v0.18.1.tar.gz
-mv litercoind ~/bin
-mv litercoin-cli ~/bin
+sudo mv litercoind /usr/local/bin
+sudo mv litercoin-cli /usr/local/bi
