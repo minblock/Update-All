ba1362f441c46d4dd443194c63e61b43848667df
stevecat <38344452+minblock@users.noreply.github.com>
2021-11-03 16:02:29 -0400

    Update updateworkcoin.sh

--- updateworkcoin.sh
+++ updateworkcoin.sh
@@ -1,5 +1,5 @@
 echo 0.18.1-RC1 Update
-wgethttps://github.com/minblock/workcoin/releases/download/0.18.1.0/Workcoin-v0.18.1.tar.gz
+wget https://github.com/minblock/workcoin/releases/download/0.18.1.0/Workcoin-v0.18.1.tar.gz
 echo "5a91f1f79d168bb710bebe333e32f76289c0e5812423a7f9e0c9812e7eab747b  Workcoin-v0.18.1.tar.gz" | sha256sum -c
 echo This should say OK if the file was unmodified during transmission
 tar -xvf Workcoin-v0.18.1.tar.gz
