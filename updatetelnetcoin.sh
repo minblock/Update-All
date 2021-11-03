e31288f8283f4c386c35e4d43e245e7474ea2e85
stevecat <38344452+minblock@users.noreply.github.com>
2021-11-03 19:33:38 -0400

    Ripple#forifthefirstwasntchangeenoughtoshis

--- updatetelnetcoin.sh
+++ updatetelnetcoin.sh
@@ -1,6 +1,6 @@
 echo 0.18.1-RC1 Update
 wget https://github.com/minblock/telnetcoin/releases/download/v0.18.1-rc3/Telnetcoin-v0.18.1-properrelease2beans.tar.gz
-echo "a0aba5204b85e7364147299693cc9ad4838d07503cf6b2940ffeef29c8c8e8e7  Telnetcoin-v0.18.1-properrelease2beans.tar.gz" | sha256sum -c
+echo "07f0047298fb16c9e2e653ad2e1be25c08e9db182d155d2a4398aed55063e03e  Telnetcoin-v0.18.1-properrelease2beans.tar.gz" | sha256sum -c
 echo This should say OK if the file was unmodified during transmission
 tar -xvf Telnetcoin-v0.18.1-properrelease2beans.tar.gz
 rm -rf  Telnetcoin-v0.18.1-properrelease2beans.tar.gz
