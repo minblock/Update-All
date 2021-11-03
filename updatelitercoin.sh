bd55386e4e920e093522ae471ed0d061a27b9e1f
stevecat <38344452+minblock@users.noreply.github.com>
2021-11-03 15:31:47 -0400

    Update updatelitercoin.sh

--- updatelitercoin.sh
+++ updatelitercoin.sh
@@ -1,6 +1,6 @@
 echo 0.18.1-RC#21 Update
 wget https://github.com/minblock/litercoin/releases/download/v0.18.1-RC2/Litercoin-v0.18.1-Properrelease.tar.gz
-echo "9bd158a1e24b6b3c648a4a981aa350bec04b3ac0c7c4dfd3ba338da1ec176de2  Litercoin-v0.18.1-Properrelease.tar.gz" | sha256sum -c
+echo "e4fd221ae777e842156a936848d0586d6f35eda03032dbabf2d96fd0a3286ffc  Litercoin-v0.18.1-Properrelease.tar.gz" | sha256sum -c
 echo This should say OK if the file was unmodified during transmission
 tar -xvf Litercoin-v0.18.1-Properrelease.tar.gz
 rm -rf  Litercoin-v0.18.1-Properrelease.tar.gz
