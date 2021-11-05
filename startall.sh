21ff4c055833b05b20df1893ff47a1535924ea0d
stevecat <38344452+minblock@users.noreply.github.com>
2021-11-05 03:58:47 -0400

    Update startall.sh

--- startall.sh
+++ startall.sh
@@ -1,9 +1,15 @@
 chmod +X startall.sh
-~/bin/picscoind -daemon
-~/bin/satellited -daemon
-~/bin/majesteesd -daemon
-~/bin/computed -daemon
-~/bin/ogkushd -daemon
-~/bin/jotod -daemon
-~/bin/luad -daemon
-~/bin/litercoind -daemon
+/usr/local/bin/picscoind 
+/usr/local/bin/satellited 
+/usr/local/bin/majesteesd 
+/usr/local/bin/computed 
+/usr/local/bin/ogkushd 
+/usr/local/bin/jotocoind 
+/usr/local/bin/luascoind 
+/usr/local/bin/litercoind 
+/usr/local/bin/workcoind 
+/usr/local/bin/telnetcoind 
+/usr/local/bin/bitcoind -daemon
+#!/usr/bin/env bash
+echo "Starting geth"
+screen -dmS geth /usr/bin/geth --verbosity 3
