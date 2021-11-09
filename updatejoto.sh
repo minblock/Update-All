d4462d3f30a8c6faa29c199cdb6dafb1e34386ad
stevecat <38344452+minblock@users.noreply.github.com>
2021-11-08 21:47:29 -0500

    Update updatejoto.sh

--- updatejoto.sh
+++ updatejoto.sh
@@ -1,14 +1,8 @@
-echo 2.0.1 Update
-mkdir ~/bin
-cd ~/bin
-rm joto*
-mkdir jototemp
-cd jototemp
-wget https://github.com/minblock/Joto/releases/download/v2.0.1-rc1/joto.tar
-tar -xvf joto.tar
-mv jotod ../
-mv joto-cli ../
-cd ../
-rm -rf jototemp
-~/bin/joto-cli s top
-~/bin/jotod -daemon
+echo Joti 2.0.1-rc3 Update
+wget https://github.com/minblock/joto/releases/download/v2.0.1-rc2/jotocoin-2.0.1.tar.gz
+echo "55c12462090fcecd576277fca70d9050d2b9a39901b2ba409ec3bf69d6b22a14 jotocoin-2.0.1.tar.gz" | sha256sum -c
+echo This should say OK if the file was unmodified during transmission
+tar -xvf jotocoin-2.0.1.tar.gz
+rm -rf  jotocoin-2.0.1.tar.gz
+sudo mv jotocoind /usr/local/bin
+sudo mv jotocoin-cli /usr/local/bin
