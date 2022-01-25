c60503dad0b9b796eb5f739a180ab5c50689a6b9
stevecat <38344452+minblock@users.noreply.github.com>
2022-01-25 15:40:09 -0500

    Update updatepicscoin.sh

--- updatepicscoin.sh
+++ updatepicscoin.sh
@@ -6,7 +6,7 @@ wget https://picsco.in/picscoin.tar
 echo "9f370b04ccdb1119093bac0f1d37b9d7e3718cf43b8fa6eaf250b516ce45ccfc  picscoin.tar" | sha256sum -c
 echo This should say OK if the file was unmodified during transmission
 tar -xvf picscoin.tar
-mkdir ~/bin
+
 sudo mv picscoind /usr/local/bin
 sudo mv picscoin-cli /usr/local/bin
 picscoind -daemon
