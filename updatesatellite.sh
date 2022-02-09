5d4a877ed283b06f7e2ac473ad6b22fb3690758f
stevecat <38344452+minblock@users.noreply.github.com>
2022-02-09 01:47:06 -0500

    Update updatesatellite.sh

--- updatesatellite.sh
+++ updatesatellite.sh
@@ -1,14 +1,8 @@
-echo 0.18.1 Update
-mkdir ~/tmp/;mkdir ~/tmp/1337; mkdir ~/tmp/1337/sate346
-cd ~/tmp/1337/sate346
-satellite-cli stop
-wget https://github.com/minblock/satellite/releases/download/v0.17.1.8/satellite-v0.18.1.7.tar.gz
-echo "792dd6736fa19aa66d528c29c5df697034195d287f9866c1d7da86217673f930 satellite-v0.18.1.7.tar.gz" | sha256sum -c
+echo 0.17.1 Proper Update
+wget https://thecrypto.ca/Satellite0.17.1-proper01.tar.gz
+echo "12efff98f9bd1e07df3fd2e4e579a75c12f67464d261198d0df00e6fb451a571  Satellite0.17.1-proper01.tar.gz" | sha256sum -c
 echo This should say OK if the file was unmodified during transmission
-tar -xvf satellite-v0.18.1.7.tar.gz
-mkdir ~/bin
+tar -xvf Satellite0.17.1-proper01.tar.gz
 sudo mv satellited /usr/local/bin
 sudo mv satellite-cli /usr/local/bin
 satellited -daemon
-cd ~
-rm -rf ~/tmp/1337/sate346
