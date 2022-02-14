22847b4935524f7ace62a8959c1e6accd0729d58
stevecat <38344452+minblock@users.noreply.github.com>
2022-02-14 11:24:38 -0500

    Update updatecompute.sh

--- updatecompute.sh
+++ updatecompute.sh
@@ -1,10 +1,9 @@
-mkdir ~/temp6549
-cd ~/temp6549
-wget https://github.com/minblock/compute/releases/download/v0.14.6.1-RC/computecore-0.14.6-x86_64-linux-gnu.tar.gz
-echo "f07e28f7912418f06b77b72dbbad929a227e84d22f3b3d5ca7abc5369319fc7a  computecore-0.14.6-x86_64-linux-gnu.tar.gz" | sha256sum -c
+wget https://putez.org/computev0.14.8.1-rc.tar.gz
+echo "e3f8a12ddf1c84bd4ec70d5ebd21cc3ae37e235094f1a1cee38f99ab2ecf1e73  computev0.14.8.1-rc.tar.gz" | sha256sum -c
 echo "This should say OK. If it doesn't check your DNS"
-tar -xvf computecore-0.14.6-x86_64-linux-gnu.tar.gz
-mkdir ~/bin/
-mv computecore-0.14.6/bin/* ~/bin
-cd ../;rm -rf ~/temp6549
-~/bin/computed -daemon
+tar -xvf computev0.14.8.1-rc.tar.gz
+sudo rm /usr/local/bin/computed
+sudo mv computed /usr/local/bin
+sudo rm /usr/local/bin/compute-cli
+sudo mv compute-cli /usr/local/bin
+computed
