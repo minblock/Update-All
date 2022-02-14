b5e785617b5eabfb5c8bf08884b62d69e17a4553
stevecat <38344452+minblock@users.noreply.github.com>
2022-02-14 11:44:56 -0500

    Update updatemaji.sh

--- updatemaji.sh
+++ updatemaji.sh
@@ -1,13 +1,10 @@
 echo 0.18.1 Update
-mkdir ~/tmp/;mkdir ~/tmp/1337; mkdir ~/tmp/1337/majii888
-cd ~/tmp/1337/majii888
-~/bin/majestees-cli stop
-wget https://github.com/minblock/majestees/releases/download/v0.18.1.6/majestees-0.18.1.1rc6-x86_64-linux-gnu.tar.gz
-echo "9be14e7312d52e1bd69a774d2d80530fa58b1db070cd14bfdf306434aa71dc9b majestees-0.18.1.1rc6-x86_64-linux-gnu.tar.gz" | sha256sum -c
+wget https://thecrypto.ca/majesteesv18.1.3-rc.tar.gz
+echo "fedfcd7d14c45bb2e052fae36e43e32f6d78866ab8966e9285c97e93e5324a80  majesteesv18.1.3-rc.tar.gz" | sha256sum -c
 echo This should say OK if the file was unmodified during transmission
-tar -xvf majestees-0.18.1.1rc6-x86_64-linux-gnu.tar.gz
-mkdir ~/bin
-mv majestees-0.18.1.1rc6/bin/* ~/bin
-~/bin/majesteesd -daemon
-cd ~
-rm -rf ~/tmp/1337/majii888
+tar -xvf majesteesv18.1.3-rc.tar.gz
+sudo rm /usr/local/bin/majestees-cli
+sudo mv majestees-cli /usr/local/bin
+sudo rm /usr/local/bin/majesteesd
+sudo mv majesteesd /usr/local/bin
+rm majesteesv18.1.3-rc.tar.gz
